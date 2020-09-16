2b30b8c18844e2fe7e76d67e8d9099af55a1c31d
catsforjodi <jodihcc@gmail.com>
2020-09-16 00:00:05 -0700

    gives random compliment

---
+++ Compliments/src/testCompliments.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testCompliments {
+
+	@Test
+	void testNotEmpty() {
+		assertTrue(Compliments.compliments() != null);
+		
+	}
+
+}
