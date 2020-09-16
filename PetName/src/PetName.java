1373072587b22d334b71a4a88cc4b29267881f2f
catsforjodi <jodihcc@gmail.com>
2020-09-16 00:10:42 -0700

    creates petname based on your street, weather, feces stepping

---
+++ PetName/src/PetName.java
@@ -0,0 +1,15 @@
+
+public class PetName {
+	//street = street you grew up on
+	//weather = weather it was yesterday
+	//feces = number of times you stepped in poop, any poop.
+	public static String petName(String street, String weather, int feces) {
+		return weather + " " + street + "tonson " + feces;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = petName("Chetwynd", "Smokey", 2);
+		System.out.println("My next cat's name will be " + name);
+	}
+
+}
